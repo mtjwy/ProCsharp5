33fc66e28c47b52841d9ce85683221f766b5db78
mtjwy <wuyucsu@gmail.com>
2015-10-25 15:30:04 -0500

    enum

--- ProCsharp5/Program.cs
+++ ProCsharp5/Program.cs
@@ -9,38 +9,70 @@ namespace ProCsharp5
     class Program
     {
         /*
-            Optional parameters
-                The value assigned to an optional parameter must be known at compile time
-                Optional parameters must always be put at the end of parameter list
-            
-            Named parameters
-                named parameters and optional arguments tend to work hand in hand.
-                named parameters allowed the called to specify only the parameters for which they
-                do not wish to received the defaults.
+            enum
+                enum is used to create a set of symbolic names that map to known numerical values
          */
+
+
+        enum Employee1
+        //default underlying storage type of enum is int
+        //enum Employee: Byte //--> set underlying storage type to Byte
+        {
+            Manager,      // = 0
+            Grunt,        // = 1
+            Contractor,   // = 2
+            VicePresident // = 3
+
+        }
+
+        //begine with 100
+        enum Employee2
+        {
+            Manager = 100,      
+            Grunt,        // = 101
+            Contractor,   // = 102
+            VicePresident // = 103
+
+        }
+
+        //Elements of enumeration need not be sequential!
+        enum Employee3
+        {
+            Manager = 100,
+            Grunt = 3,        
+            Contractor = 39,   
+            VicePresident = 20
+
+        }
+
+        static void GetBonusByEmployeeType(Employee1 e)
+        {
+            switch (e)
+            {
+                case Employee1.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                    break;
+                case Employee1.Grunt:
+                    Console.WriteLine("You have got to be kidding ... ");
+                    break;
+                case Employee1.Contractor:
+                    Console.WriteLine("You already get enough cash ...");
+                    break;
+                case Employee1.VicePresident:
+                    Console.WriteLine("Very good! Sir!");
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
 
-            DisplayFancyMessage();
-            DisplayFancyMessage(message: "Hello");
+            GetBonusByEmployeeType(Employee1.VicePresident);
+            GetBonusByEmployeeType(Employee1.Contractor);
             Console.ReadLine();
 
         }
 
-        static void DisplayFancyMessage(ConsoleColor textColor = ConsoleColor.Blue,
-                                        ConsoleColor backgroundColor = ConsoleColor.White,
-                                        string message = "Test Message")
-        {
-            // Store old colors to restore after message is printed. 
-            ConsoleColor oldTextColor = Console.ForegroundColor;
-            ConsoleColor oldbackgroundColor = Console.BackgroundColor;
-            // Set new colors and print message. 
-            Console.ForegroundColor = textColor;
-            Console.BackgroundColor = backgroundColor;
-            Console.WriteLine(message);
-            // Restore previous colors. 
-            Console.ForegroundColor = oldTextColor;
-            Console.BackgroundColor = oldbackgroundColor;
-        }
+        
     }
 }
