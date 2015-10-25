7e9d269726108201558ff35267418396967edd70
mtjwy <wuyucsu@gmail.com>
2015-10-25 18:16:16 -0500

    Passing Reference Types by Reference

--- ProCsharp5/Program.cs
+++ ProCsharp5/Program.cs
@@ -9,23 +9,54 @@ namespace ProCsharp5
     class Program
     {
         /*
-            Value Types Containing Reference Types
-                By default, when a value type contains other reference types, 
-                assignment results in a copy of the references. 
-
-                When you want to perform a deep copy, one approach is to 
-                implement the ICloneable interface 
-                
+            Passing Reference Types by Reference
+               If a reference type is passed by reference, 
+               the callee can change the object the original reference is referencing.      
          */
 
+        //pass by ref
+        static void SendAPersonByReference(ref Person p)
+        {
+            // Change some data of "p". 
+            p.personAge = 555;
 
+            // "p" is now pointing to a new object on the heap! 
+            p = new Person("Nikki", 999);
+        }
 
-
+        class Person
+        {
+            public string personName;
+            public int personAge;
+            // Constructors. 
+            public Person(string name, int age)
+            {
+                personName = name;
+                personAge = age;
+            }
+            public Person() { }
+            public void Display()
+            {
+                Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);
+            }
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Passing Person object by reference *****");
+            Person mel = new Person("Mel", 23);
+            Console.WriteLine("Before passing by ref call, Person is:");
+            mel.Display();
 
-            
+            Console.WriteLine();
+
+            // Passing ref-types by ref
+            SendAPersonByReference(ref mel);
+            Console.WriteLine("After passing by ref call, Person is:");
+            mel.Display();
+
+            Console.ReadLine();
+
 
             Console.ReadLine();
 
