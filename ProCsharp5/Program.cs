89f5c0f5d50d84967d9d70bc1bc64c9c254d0d62
mtjwy <wuyucsu@gmail.com>
2015-10-24 23:50:21 -0500

    System.Environment class

--- ProCsharp5/Program.cs
+++ ProCsharp5/Program.cs
@@ -11,13 +11,26 @@ namespace ProCsharp5
         //Main method is private by default
         static int Main(string[] args)
         {
-            //processing any incoming args
-            foreach(string arg in args)
-            {
-                Console.Write("Arg: {0}", arg);
-            }
+            ShowEnvironmentDetails();
             Console.ReadLine();
             return -1;
+        }
+
+        //use some methods in System.Environment
+        static void ShowEnvironmentDetails()
+        {
+            foreach(String drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Driver: {0}", drive);
+            }
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET Version: {0}", Environment.Version);
+            Console.WriteLine("User name: {0}", Environment.UserName);
+            Console.WriteLine("Machine name: {0}", Environment.MachineName);
+            Console.WriteLine("New line: {0}", Environment.NewLine);
+            Console.WriteLine("System directory: {0}", Environment.SystemDirectory);
+          
         }
     }
 }
