10fa5e2e42dc9835937da663367396f0094bd4cb
mtjwy <wuyucsu@gmail.com>
2015-10-26 01:22:21 -0500

    static class

---
+++ ProCsharp5/TimeUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCsharp5
+{
+    /*
+        static class
+            When designing a utility class, it is good practice to apply the static keyword to the class definition.
+            
+            static class can only contain static members. 
+     */
+
+    // static class can only contain static members. 
+    static class TimeUtil
+    {
+        public static void PrintTime()
+        { Console.WriteLine(DateTime.Now.ToShortTimeString()); }
+
+        public static void PrintDate()
+        { Console.WriteLine(DateTime.Today.ToShortDateString()); }
+    }
+}
