5763eae0123ae8a1d709fc940c9b36649f64078c
mtjwy <wuyucsu@gmail.com>
2015-10-26 21:08:35 -0500

    Constant field data and Read-only field data

--- ProCsharp5/Program.cs
+++ ProCsharp5/Program.cs
@@ -8,18 +8,28 @@ namespace ProCsharp5
 {
     class Program
     {
-       
+        /*
+            Constant Field Data 
+                public const double PI = 3.14;
+                Constant Field Data can never change after the initial assignment.
+                Constant Field Data is implicitly static.
+                Constant Field Data must be known at compile time. 
+                the initial value assigned to the constant must be specified at the time you define the constant.
+
+            Read-only field data
+                public readonly double PI;
+                read-only field cannot be changed after the initial assignment.
+                the value assigned to a read-only field can be determined at runtime.
+                not implicitly static.
+                can legally be assigned within the scope of a constructor, but nowhere else.
+
+
+         */
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine("***** Automatic Properties *****\n");
-            Car c = new Car();
-            c.PetName = "Frank";
-            c.Speed = 55;
-            c.Color = "Red";
-            Console.WriteLine("Your car is named {0}? That's odd...",
-            c.PetName);
-            c.DisplayStats();
+            
 
             Console.ReadLine();
             
